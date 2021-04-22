fbb5a13f29603c213d4fc3ce38e44f440a7229bc
Ossian Däckfors <ossian.dackfors@gmail.com>
2021-04-22 15:29:40 +0200

    Spectacular Death

--- MAPP2021/Assets/Script/PlayerDeath.cs
+++ MAPP2021/Assets/Script/PlayerDeath.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 public class PlayerDeath : MonoBehaviour
 {
     [SerializeField] private GameOver gameOver;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private ParticleSystem particleSystem;
+    [SerializeField] private int timeStopTime;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         gameOver.setAlive(false);
+        var main = particleSystem.main;
+        main.useUnscaledTime = true;
+        particleSystem.Play();
+        spriteRenderer.enabled = false;
         Time.timeScale = 0f;
+
     }
+
+
 }
