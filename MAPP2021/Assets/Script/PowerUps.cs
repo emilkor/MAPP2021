a5e5bb51012cc4ece91aeb179f12891da843a037
Ossian Däckfors <ossian.dackfors@gmail.com>
2021-04-26 10:28:46 +0200

    Nya block breaker aktiverad

--- MAPP2021/Assets/Script/PowerUps.cs
+++ MAPP2021/Assets/Script/PowerUps.cs
@@ -38,7 +38,7 @@ public class PowerUps : MonoBehaviour
         }
         if(powerUp == PowerUp.WallBreak)
         {
-            StartCoroutine(WallBreak());
+            WallBreak();
         }
 
 
@@ -57,33 +57,32 @@ public class PowerUps : MonoBehaviour
         yield return new WaitForSeconds(SuperSpeedTime);
         playerMovement.ResetMovementSpeed();
     }
-    private IEnumerator WallBreak()
+    //private IEnumerator WallBreak()
+    //{
+    //    playerCollider.enabled = false;
+    //    blockDestroier.SetActive(true);
+    //    yield return new WaitForSeconds(.2f);
+    //    blockDestroier.SetActive(false);
+    //    playerCollider.enabled = true;
+    //}
+    private void WallBreak()
     {
-        playerCollider.enabled = false;
-        blockDestroier.SetActive(true);
-        yield return new WaitForSeconds(.2f);
-        blockDestroier.SetActive(false);
-        playerCollider.enabled = true;
+        RaycastHit2D raycast = Physics2D.Raycast(gameObject.transform.position + new Vector3(0, .5f), Vector2.up, 50);
+        StartCoroutine(DestroyLight(raycast.distance));
+        if (raycast.collider != null)
+        {
+            Destroy(raycast.transform.gameObject);
+        }
     }
-    //private void WallBreak()
-    //{
-    //    RaycastHit2D raycast = Physics2D.Raycast(gameObject.transform.position + new Vector3(0 , .5f), Vector2.up, 50);
 
-    //    destroyLight.SetActive(true);
-    //    StartCoroutine(DestroyLight(raycast.distance));
-    //    if (raycast.collider != null)
-    //    {
-    //        Destroy(raycast.transform.gameObject);
-    //    }
-    //}
-
-    //private IEnumerator DestroyLight(float distance)
-    //{
-    //    destroyLight.transform.localScale = new Vector3(.1f, distance);
-    //    destroyLight.transform.localPosition = new Vector3(0, .5f + (distance / 2));
-    //    yield return new WaitForSeconds(.05f);
-    //    destroyLight.SetActive(false);
-    //}
+    private IEnumerator DestroyLight(float distance)
+    {
+        destroyLight.transform.localScale = new Vector3(.1f, distance);
+        destroyLight.transform.localPosition = new Vector3(0, .5f + (distance / 2));
+        destroyLight.SetActive(true);
+        yield return new WaitForSeconds(.05f);
+        destroyLight.SetActive(false);
+    }
 
 
 }
