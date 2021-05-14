70cb4c85d14088bff7b0edc9ddb145f65ea74101
emilkor <emko9936@student.su.se>
2021-05-14 14:10:17 +0200

    Färger fixade- start

--- MAPP2021/Assets/Script/ChangePlayerColor.cs
+++ MAPP2021/Assets/Script/ChangePlayerColor.cs
@@ -6,51 +6,95 @@ using UnityEngine.UI;
 public class ChangePlayerColor : MonoBehaviour
 {
     private static Color newColor;
-    [SerializeField] private Button buttonOne;
-    [SerializeField] private Button buttonTwo;
+
+
+
+    [SerializeField] private Button[] buttons;
+    [SerializeField] private Text[] text;
+
     private int highscore;
 
 
-    /* 
-     private void Awake()
-      {
-          DontDestroyOnLoad(gameObject);
-      }
-
-      */
-
     private void Awake()
     {
+        SetButtons();
+
+        PlayerPrefs.SetInt("HighScore", 1000000);
+
         highscore = PlayerPrefs.GetInt("HighScore");
 
-        if(highscore >= 1000)
+        if(highscore >= 0)
         {
-            buttonOne.enabled = true;
+            buttons[0].interactable = true;
+            text[0].enabled = false;
         }
 
-        if(highscore >= 3000)
+        if(highscore >= 2000)
         {
-            buttonTwo.enabled = true; 
+            buttons[1].interactable = true;
+            text[1].enabled = false;
+           
         }
+
+        if(highscore >= 4000)
+        {
+            buttons[2].interactable = true;
+            text[2].enabled = false;
+           
+        }
+
+        if(highscore >= 6000)
+        {
+            buttons[3].interactable = true;
+            text[3].enabled = false;
+            
+        }
+
+        if(highscore >= 8000)
+        {
+            buttons[4].interactable = true;
+            text[4].enabled = false;
+         
+        }
+
+    }
+
+    private void SetButtons()
+    {
+
+        foreach (Button button in buttons) {
+            button.interactable = false;
+        }
+       
     }
     private void Start()
     {
-        buttonOne.enabled = false;
-        buttonTwo.enabled = false;
+       
         Debug.Log(PlayerPrefs.GetInt("HighScore"));
     }
 
 
-    public static void ChangeColorToBlue()
+    public static void White()
+    {
+        newColor = Color.white;
+    }
+
+    public static void Red()
+    {
+        newColor = Color.red;
+    }
+
+    public static void Blue()
     {
         newColor = Color.blue;
 
     }
 
-    public static void ChangeColorToRed()
+    public static void Green()
     {
-        newColor = Color.red;
+        newColor = Color.green;
     }
+
 
     public static Color GetColor()
     {
