a44f47b171d953ad2c92940761b68dd3595b8537
emilkor <emko9936@student.su.se>
2021-05-14 10:56:30 +0200

    UI

---
+++ MAPP2021/Assets/Script/ChangeColorsOnPlayer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorsOnPlayer : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private TrailRenderer trail;
+   // [SerializeField] private P
+}
