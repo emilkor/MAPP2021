230dbfd7175075185c85bc5355f3b9debae3039e
Ossian Däckfors <ossian.dackfors@gmail.com>
2021-05-18 09:09:31 +0200

    clamp

--- MAPP2021/Assets/Script/ButtonClamp.cs
+++ MAPP2021/Assets/Script/ButtonClamp.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.UI;
 using Vector2 = UnityEngine.Vector2;
 
 public class ButtonClamp : MonoBehaviour
 {
+    [SerializeField] private ScrollRect scroll;
     [SerializeField] private RectTransform gameObjectRect;
     [SerializeField] private RectTransform[] buttonsRect;
+    
 
     private float lowestRect;
     private float highestRect;
@@ -41,6 +44,10 @@ public class ButtonClamp : MonoBehaviour
     {
         anchoredPos = gameObjectRect.anchoredPosition;
         xPos = anchoredPos.x;
+        if(xPos < -highestRect || xPos > lowestRect)
+        {
+            scroll.StopMovement();
+        }
         xPos = Mathf.Clamp(xPos, -highestRect, lowestRect);
         anchoredPos.x = xPos;
         gameObjectRect.anchoredPosition = anchoredPos;
