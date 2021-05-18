cc11996b301485aa13fef58e17a25bdeb6cb933d
emilkor <emko9936@student.su.se>
2021-05-18 11:17:21 +0200

    Färg sparning funkar!

--- MAPP2021/Assets/Script/ChangePlayerColor.cs
+++ MAPP2021/Assets/Script/ChangePlayerColor.cs
@@ -10,7 +10,7 @@ public class ChangePlayerColor : MonoBehaviour
 
 
     [SerializeField] private Button[] buttons;
-  
+
     private int highscore;
 
     private int unlocks = 1;
@@ -24,11 +24,11 @@ public class ChangePlayerColor : MonoBehaviour
         SetButtons();
 
         //VVV Ta bort sen /August
-       // PlayerPrefs.SetInt("HighScore", 0);
+        // PlayerPrefs.SetInt("HighScore", 0);
 
         highscore = PlayerPrefs.GetInt("HighScore");
 
-        while(highscore >= unlockThreshold)
+        while (highscore >= unlockThreshold)
         {
             unlocks++;
 
@@ -37,7 +37,7 @@ public class ChangePlayerColor : MonoBehaviour
         }
 
         //Alla knappar måste ha ett objekt som heter "Lock" som ligger på index 1 för att detta ska funka /August
-        for(int i = 0; i < unlocks; i++)
+        for (int i = 0; i < unlocks; i++)
         {
             buttons[i].interactable = true;
             GameObject lockImage = buttons[i].transform.GetChild(1).gameObject;
@@ -59,12 +59,12 @@ public class ChangePlayerColor : MonoBehaviour
             buttonValue *= thresholdMultiplier;
             buttonText.text = buttonValue.ToString();
 
-            if(buttonValue >= 10000)
+            if (buttonValue >= 10000)
             {
                 buttonText.fontSize -= 2;
             }
 
-            if(buttonValue >= 100000)
+            if (buttonValue >= 100000)
             {
                 buttonText.fontSize -= 2;
             }
@@ -74,81 +74,102 @@ public class ChangePlayerColor : MonoBehaviour
     private void SetButtons()
     {
 
-        foreach (Button button in buttons) {
+        foreach (Button button in buttons)
+        {
             button.interactable = false;
         }
-       
+
     }
     private void Start()
     {
         Debug.Log(PlayerPrefs.GetInt("HighScore"));
+        print(PlayerPrefs.GetString("PlayerColor"));
     }
 
 
     public static void White()
     {
         newColor = Color.white;
-        
+        SaveColor();
+
     }
 
     public static void Red()
     {
         newColor = Color.red;
-        
+        SaveColor();
+
     }
 
     public static void DiffusedRed()
     {
         newColor = new Color(1f, 0.3f, 0.3f, 1f);
-        
+        SaveColor();
+
     }
 
     public static void Blue()
     {
         newColor = Color.blue;
+        SaveColor();
 
     }
 
     public static void Green()
     {
         newColor = Color.green;
+        SaveColor();
     }
 
     public static void Pink()
     {
         newColor = new Color(1, 0, 1, 1);
+        SaveColor();
     }
 
     public static void Cyan()
     {
         newColor = new Color(0, 1, 1, 1);
+        SaveColor();
     }
 
     public static void Yellow()
     {
         newColor = new Color(1, 1, 0, 1);
+        SaveColor();
     }
 
     public static void Purple()
     {
         newColor = new Color(0.5f, 0, 0.5f, 1);
+        SaveColor();
     }
 
     public static void RandomColor()
     {
         newColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        SaveColor();
     }
 
 
     public static Color GetColor()
     {
-        if(newColor.Equals(Color.clear))
+        /*
+        if (newColor.Equals(Color.clear))
         {
             White();
         }
+        */
+        Color color;
 
-        return newColor;
+        ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("PlayerColor"), out color);
+
+        return color;
     }
 
-   
+    public static void SaveColor()
+    {
+        PlayerPrefs.SetString("PlayerColor", ColorUtility.ToHtmlStringRGBA(newColor));
+        PlayerPrefs.Save();
+    }
 }
