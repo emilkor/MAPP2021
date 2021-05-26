380454fb0db0719c66b35bee9d19b1c6b193e738
yaouOps <72212799+yaouOps@users.noreply.github.com>
2021-05-26 11:36:04 +0200

    cleanup + master

--- MAPP2021/Assets/Script/ChangePlayerColor.cs
+++ MAPP2021/Assets/Script/ChangePlayerColor.cs
@@ -25,7 +25,7 @@ public class ChangePlayerColor : MonoBehaviour
        
 
         //VVV Ta bort sen /August
-        PlayerPrefs.SetInt("HighScore", 10000);
+        //PlayerPrefs.SetInt("HighScore", 10000000);
 
         highscore = PlayerPrefs.GetInt("HighScore");
 
